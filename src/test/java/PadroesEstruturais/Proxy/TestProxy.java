933c335d1fa008467a253391e8ff7fd42d3a40f6
JotaVS <jotavs.dleid@gmail.com>
2024-06-18 16:06:57 -0300

    TestProxy update

--- src/test/java/PadroesEstruturais/Proxy/TestProxy.java
+++ src/test/java/PadroesEstruturais/Proxy/TestProxy.java
@@ -10,32 +10,40 @@ public class TestProxy {
     @Test
     public void testeDepositarSenhaCorreta() {
         IContaBancaria contaBancaria = new ContaBancariaProxy(1000.0, "123");
-        String response = contaBancaria.depositar(200.0, "123");
-        assertEquals("Depositou: 200.0", response);
+        String resultado = contaBancaria.depositar(200.0, "123");
+        assertEquals("Depositou: 200.0", resultado);
         assertEquals(1200.0, contaBancaria.getSaldo(), 0.001);
     }
 
     @Test
     public void testeDepositarSenhaIncorreta() {
         IContaBancaria contaBancaria = new ContaBancariaProxy(1000.0, "123");
-        String response = contaBancaria.depositar(200.0, "456");
-        assertEquals("Acesso negado. Senha Incorreta.", response);
+        String resultado = contaBancaria.depositar(200.0, "456");
+        assertEquals("Acesso negado. Senha Incorreta.", resultado);
         assertEquals(1000.0, contaBancaria.getSaldo(),0.001);
     }
 
     @Test
     public void testeRetirarSenhaCorreta() {
         IContaBancaria contaBancaria = new ContaBancariaProxy(1000.0, "123");
-        String response = contaBancaria.retirar(200.0, "123");
-        assertEquals("Retirou: 200.0", response);
+        String resultado = contaBancaria.retirar(200.0, "123");
+        assertEquals("Retirou: 200.0", resultado);
         assertEquals(800.0, contaBancaria.getSaldo(),0.001);
     }
 
     @Test
     public void testeRetirarSenhaIncorreta() {
         IContaBancaria contaBancaria = new ContaBancariaProxy(1000.0, "123");
-        String response = contaBancaria.retirar(200.0, "456");
-        assertEquals("Acesso negado. Senha Incorreta.", response);
+        String resultado = contaBancaria.retirar(200.0, "456");
+        assertEquals("Acesso negado. Senha Incorreta.", resultado);
+        assertEquals(1000.0, contaBancaria.getSaldo(),0.001);
+    }
+
+    @Test
+    public void testeRetirarSaldoInsuficiente(){
+        IContaBancaria contaBancaria = new ContaBancariaProxy(1000.0, "123");
+        String resultado = contaBancaria.retirar(1200.0, "123");
+        assertEquals("Saldo Insuficiente.", resultado);
         assertEquals(1000.0, contaBancaria.getSaldo(),0.001);
     }
 
